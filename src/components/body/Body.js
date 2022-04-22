cc12064542d2a04be158aaf65359a83fe6f0d489
Leonardo.muñoz <leonardoMoc11@gmail.com>
2022-04-22 16:15:18 -0500

    update body component

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -14,9 +14,9 @@ export const Body = () => {
 
     const handledOnShowTable = () => setIsShowHistory(!isShowHistory);
 
-    const accesLogin = async () => {
-        if (name) {
-            console.log(name);
+    const searchName = async () => {
+        let regex = new RegExp("^[a-zA-Z]+$");
+        if (regex.test(name.data)) {
             const requestOptions = {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json; charset=utf-8' },
@@ -27,8 +27,9 @@ export const Body = () => {
                     console.log(data.country[0]);
                     setResponse(data);
                     setIsShowTable(true);
-                });
-            history.push(name);
+                    history.push(name);
+                })
+                .catch(error => alert('Invalid value, say a real name'));
         } else {
             alert('You need to fill in the name field');
         }
@@ -40,24 +41,26 @@ export const Body = () => {
                 <div className='flex-1 flex flex-row'>
                     <button onClick={handledOnShowTable} className='w-28 bg-slate-600 h-12 rounded text-white font-medium'>View History</button>
                     {isShowHistory && history.length > 0 &&
-                        <table class="w-52 text-sm text-left text-gray-500 dark:text-gray-400 ml-2">
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <tr>
-                                    <th scope="col" class="px-6 py-3">
-                                        Names searched
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 flex flex-col">
-                                    {history.map(item => (
-                                        <td class="px-6 py-4">
-                                            {item.data}
-                                        </td>
-                                    ))}
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div className="relative overflow-x-auto max-h-60">
+                            <table className="w-52 text-sm text-left text-gray-500 dark:text-gray-400 ml-2">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            Names searched
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 flex flex-col">
+                                        {history.map(item => (
+                                            <td className="px-6 py-4">
+                                                {item.data}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     }
                 </div>
                 <div className='flex-1'>
@@ -71,32 +74,32 @@ export const Body = () => {
                     Name:
                     <input type="text" placeholder='Search for a name' onChange={(e) => setName({ data: e.target.value })} className='border-black border ml-2 pl-2 rounded-md w-72 mr-2 h-8 justify-center font-medium' />
                 </label>
-                <button onClick={accesLogin} className='rounded-md bg-gray-700 w-20 text-white h-8 font-medium'>Search</button>
+                <button onClick={searchName} className='rounded-md bg-gray-700 w-20 text-white h-8 font-medium'>Search</button>
             </div>
 
             {isShowTable &&
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg container mx-auto mt-10">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg container mx-auto mt-10">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" className="px-6 py-3">
                                     Name
                                 </th>
                                 {response.country.map(item => (
-                                    <th scope="col" class="px-6 py-3">
+                                    <th scope="col" className="px-6 py-3">
                                         {item.country_id}
                                     </th>
                                 ))}
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
                                     {response.name}
                                 </th>
                                 {response.country.map(item => (
-                                    <td class="px-6 py-4">
-                                        {item.probability.toString().substr(0,6)}%
+                                    <td className="px-6 py-4">
+                                        {item.probability.toString().substr(0, 6)}%
                                     </td>
                                 ))}
                             </tr>
