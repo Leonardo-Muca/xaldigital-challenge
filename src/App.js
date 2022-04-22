d8dcc83191f017a4dd46f054c70076149b99513e
Leonardo.muñoz <leonardoMoc11@gmail.com>
2022-04-22 16:44:35 -0500

    update header folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/country/Header';
+import { Header } from './components/header/Header';
 import { Body } from './components/body/Body';
 import { Footer } from './components/footer/Footer';
 
